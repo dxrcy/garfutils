00548592f359a450d4599c07ccac1b90a22c9c70
darcy <acednes@gmail.com>
2024-11-20 17:30:54 +1100

    feat: switch to supporting Hyprland only

--- src/commands.rs
+++ src/commands.rs
@@ -53,39 +53,29 @@ pub fn open_editor(path: impl AsRef<OsStr>) -> Result<()> {
 
 /// BSPWM-specific functionality
 pub fn setup_image_viewer_window(paths: &[impl AsRef<OsStr>], viewer_class: &str) -> Result<()> {
-    // Window ID of main window (terminal)
-    let bspc_node = bspc_command(&["query", "-N", "-n"])?.stdout;
-    let bspc_node = std::str::from_utf8(&bspc_node)
-        .expect("commmand result should be utf-8")
-        .trim();
-
-    // Temporary hide currently focused window
-    // To avoid attaching image viewer to `tabbed` instance
-    bspc_command(&["node", bspc_node, "-g", "hidden"])?;
-
     spawn_image_viewer(paths, viewer_class, false)?;
+
     // Wait for image viewer to completely start
     // TODO(fix): Spin until image viewer window has spawned
     thread::sleep(Duration::from_millis(100));
 
-    // Unhide main window
-    // Move image viewer to left, resize slightly, re-focus main window
-    bspc_command(&["node", bspc_node, "-g", "hidden"])?;
-    bspc_command(&["node", "-s", "west"])?;
-    bspc_command(&["node", "-z", "right", "-200", "0"])?;
-    bspc_command(&["node", "-f", "east"])?;
+    // // Move image viewer to left, resize slightly, re-focus main window
+    hyprctl_command(&["swapwindow", "l"])?;
+    hyprctl_command(&["resizeactive", "-350", "0"])?;
+    hyprctl_command(&["movefocus", "r"])?;
 
     Ok(())
 }
 
-fn bspc_command(args: &[impl AsRef<OsStr>]) -> Result<process::Output> {
-    let output = Command::new("bspc")
+fn hyprctl_command(args: &[impl AsRef<OsStr>]) -> Result<process::Output> {
+    let output = Command::new("hyprctl")
+        .arg("dispatch")
         .args(args)
         .output()
-        .with_context(|| format!("Run command `bspc {}`", stringify_args(args)))?;
+        .with_context(|| format!("Run command `hypr dispatch {}`", stringify_args(args)))?;
     if !output.status.success() {
         bail!(
-            "Command did not exit successfully: `bspc {}`",
+            "Command did not exit successfully: `hypr dispatch {}`",
             stringify_args(args)
         );
     }
