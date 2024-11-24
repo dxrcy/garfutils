3b86f8872eb6bae49d3f7ddc045f4f5bd412f549
darcy <acednes@gmail.com>
2024-11-24 19:11:03 +1100

    fix: remove unnecessary return-value hack

--- src/random.rs
+++ src/random.rs
@@ -17,11 +17,9 @@ pub fn with_rng<F, T>(func: F) -> T
 where
     F: FnOnce(&mut Rng) -> T,
 {
-    let mut ret: Option<T> = None;
     RNG.with(|rng| {
         let mut rng = rng.borrow_mut();
         let rng = rng.as_mut().expect("rng should have been initialized");
-        ret = Some(func(rng));
-    });
-    ret.expect("should have been initialized with closure return value")
+        func(rng)
+    })
 }
